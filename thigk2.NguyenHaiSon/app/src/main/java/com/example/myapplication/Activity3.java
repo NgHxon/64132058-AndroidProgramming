688b179811bfabf494b9c8177d8ed329c33d4aa5
NgHxon <son.nh.64cntt@ntu.edu.vn>
2025-04-21 21:45:34 +0700

    .

--- thigk2.NguyenHaiSon/app/src/main/java/com/example/myapplication/Activity3.java
+++ thigk2.NguyenHaiSon/app/src/main/java/com/example/myapplication/Activity3.java
@@ -1,32 +1,34 @@
-package com.example.myapplication;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
-public class Activity3 extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main);
 
-        });
-        ListView = findViewById()
-        String[] subjects = {
-                "Tin học đại cương", "Lập trình Java", "Phát triển ứng dụng Web", "khai phá dữ liệu lớn", "Kinh tế chính trị"
-        };
+        // Tạo danh sách môn học
+        ArrayList<String> subjects = new ArrayList<>();
+        subjects.add("Tin học đại cương");
+        subjects.add("lập trình java");
+        subjects.add("Phát triển ứng dụng web");
+        subjects.add("khai phá dữ liệu lớn");
+        subjects.add(" kinh tế chính trị");
 
+        // Ánh xạ ListView
+        ListView listView = findViewById(R.id.listView);
+
+        // Adapter để hiển thị dữ liệu
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                subjects
+        );
+
+        // Gán adapter vào ListView
+        listView.setAdapter(adapter);
     }
-
 }
